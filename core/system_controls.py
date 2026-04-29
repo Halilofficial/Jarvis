4daa0cbdd94565cea66aefed79393569eccea10c
Halilofficial <145876440+Halilofficial@users.noreply.github.com>
2026-04-29 14:29:31 +0300

    Create system_controls.py file in core folder

---
+++ core/system_controls.py
@@ -0,0 +1,16 @@
+# System Controls
+
+# This file will handle the system controls for the Jarvis system.
+
+class SystemControls:
+    def __init__(self):
+        pass
+
+    def shutdown_system(self):
+        pass
+
+    def restart_system(self):
+        pass
+
+    def lock_screen(self):
+        pass
