caee3a3c41d2476629e15399a7ad6ef028682bb3
Halilofficial <145876440+Halilofficial@users.noreply.github.com>
2026-04-29 13:54:17 +0300

    Create audio_manager.py file

---
+++ core/audio_manager.py
@@ -0,0 +1,13 @@
+# Audio Manager
+
+# This file will handle the audio management for the system.
+
+class AudioManager:
+    def __init__(self):
+        pass
+
+    def play_sound(self, sound_file):
+        pass
+
+    def stop_sound(self):
+        pass
